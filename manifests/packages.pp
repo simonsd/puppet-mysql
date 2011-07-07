c5d0ae3acc8ee46df959ead727e99d343a5d8c53
Dave Simons <dave@inuits.be>
2011-07-07 20:28:12 +0200

    add $hardwaremodel for packages
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,21 +2,21 @@ class mysql::packages {
 	package {
 		'mysql-server':
 			ensure => installed,
-			name => 'mysql-server';
+			name => "mysql-server.$hardwaremodel";
 
 		'mysql-client':
 			ensure => installed,
 			name => $operatingsystem ? {
-				Debian => 'mysql-client',
-				Centos => 'mysql',
+				Debian => "mysql-client.$hardwaremodel",
+				Centos => "mysql.$hardwaremodel",
 			},
 			require => Package['mysql-server'];
 
 		'mysql-dev':
 			ensure => installed,
 			name => $operatingsystem ? {
-				Debian => 'libmysqlclient15-dev',
-				Centos => 'mysql-devel',
+				Debian => "libmysqlclient15-dev.$hardwaremodel",
+				Centos => "mysql-devel.$hardwaremodel",
 			},
 			require => Package['mysql-server'];
 	}
