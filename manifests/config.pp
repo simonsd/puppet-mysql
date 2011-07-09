61cbbfef91d52e165c05a5ee85a9da5f4921aeca
Dave Simons <dave@inuits.be>
2011-07-09 17:26:32 +0200

    clean up code for future additions
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,8 @@
 class mysql::config {
-	exec { 'setup_mysql_pass_root':
-		command => "/usr/bin/mysqladmin -uroot -h localhost password $mysql_rootpass",
-		unless => '/usr/bin/mysql -uroot -h localhost',
-		require => Class['mysql_packages'],
+	exec {
+		'setup_mysql_pass_root':
+			command => "/usr/bin/mysqladmin -uroot -h localhost password $mysql_rootpass",
+			onlyif => '/usr/bin/mysql -uroot -h localhost',
+			require => Class['mysql_packages'];
 	}
 }
