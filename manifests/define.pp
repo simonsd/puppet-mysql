9a21ab9adce41d56d06a8566eae4dd98ee7752ce
Dave Simons <dave@inuits.be>
2011-07-19 17:06:51 +0200

    some quoting fixes
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/define.pp
+++ manifests/define.pp
@@ -6,8 +6,9 @@ define mysql_db (
 ){
 	exec {
 		'add_db':
-			command => "mysqladmin -u$user -p$pass -h $host create $name",
-			unless => "mysql -u$user -p$pass -h $host -e 'show databases'|grep $name",
+			command => "mysqladmin -u\"$user\" -p\"$pass\" -h \"$host\" create \"$name\"",
+			path => '/usr/bin:/bin:/sbin',
+			unless => "mysql -u\"$user\" -p\"$pass\" -h \"$host\" -e 'show databases'|grep \"$name\"",
 	}
 }
 
@@ -20,7 +21,8 @@ define mysql_user (
 ){
 	exec {
 		'add_user':
-			command => "mysql -uroot -p$mysql_rootpass -h $host -e \"create user $name@$host identified by '$pass'\"",
+			command => "mysql -uroot -p\"$mysql_rootpass\" -h \"$host\" -e \"create user '$name'@'$host' identified by '$pass'\"",
+			path => '/usr/bin:/bin:/sbin',
 			unless => "mysql -u$name -p$pass -h $host";
 	}
 }
