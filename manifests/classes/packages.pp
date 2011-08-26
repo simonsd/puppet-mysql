fb21289faa7b1af38e29015e4def8dbd3dfa7bcf
Dave Simons <dave@inuits.be>
2011-08-26 13:22:10 -0700

    use fqvars
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -2,26 +2,26 @@ class mysql::packages {
 	@package {
 		'mysql-server':
 			ensure => installed,
-			name => $operatingsystem ? {
+			name => $::operatingsystem ? {
 				archlinux => 'mysql',
-				'Centos' => "mysql-server.$hardwaremodel",
+				'Centos' => "mysql-server.$::hardwaremodel",
 				'Debian' => 'mysql-server',
 			};
 
 		'mysql-client':
 			ensure => installed,
-			name => $operatingsystem ? {
+			name => $::operatingsystem ? {
 				archlinux => 'mysql-clients',
-				Centos => "mysql.$hardwaremodel",
+				Centos => "mysql.$::hardwaremodel",
 				Debian => "mysql-client",
 			},
 			require => Package['mysql-server'];
 
 		'mysql-dev':
 			ensure => installed,
-			name => $operatingsystem ? {
+			name => $::operatingsystem ? {
 				Debian => "libmysqlclient15-dev",
-				Centos => "mysql-devel.$hardwaremodel",
+				Centos => "mysql-devel.$::hardwaremodel",
 			},
 			require => Package['mysql-server'];
 	}
