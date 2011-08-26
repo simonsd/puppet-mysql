ef7217764e29dd61ed2c430e5ea18244e69d2246
Dave Simons <dave@inuits.be>
2011-08-26 16:10:39 -0700

    arch compat, syntax cleanup, fqvars - services.pp
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/service.pp
+++ manifests/classes/service.pp
@@ -2,10 +2,17 @@ class mysql::service {
 	service { 'mysqld':
 		ensure => running,
 		enable => true,
-		name => $operatingsystem ? {
-			Centos => 'mysqld',
-			Debian => 'mysql',
-			archlinux => 'mysqld',
+		name => $::operatingsystem ? {
+			default => 'mysqld',
+			debian => 'mysql',
+		},
+		path => $::operatingsystem ? {
+			default => '/etc/init.d',
+			archlinux => '/etc/rc.d',
+		},
+		hasstatus => $::operatingsystem ? {
+			default => true,
+			archlinux => false,
 		},
 		require => Package['mysql-server'];
 	}
