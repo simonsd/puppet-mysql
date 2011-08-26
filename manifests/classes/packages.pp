e4472b31afb5d9d662732b945174a236bc6bc79c
Dave Simons <dave@inuits.be>
2011-08-26 13:21:14 -0700

    virtualize pkgs, don't realize mysql-devel on arch
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -1,5 +1,5 @@
 class mysql::packages {
-	package {
+	@package {
 		'mysql-server':
 			ensure => installed,
 			name => $operatingsystem ? {
@@ -20,10 +20,13 @@ class mysql::packages {
 		'mysql-dev':
 			ensure => installed,
 			name => $operatingsystem ? {
-				archlinux => undef,
 				Debian => "libmysqlclient15-dev",
 				Centos => "mysql-devel.$hardwaremodel",
 			},
 			require => Package['mysql-server'];
 	}
+
+	realize(Package['mysql-server', 'mysql-client'])
+	if $::operatingsystem != 'archlinux' {
+		realize(Package['mysql-dev'])
 }
