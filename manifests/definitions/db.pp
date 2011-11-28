191e8914ca0dbddeb544d8884715aada2337be39
Dave Simons <dave@inuits.eu>
2011-11-28 02:06:42 +0100

    no need for the pass param to the db def
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/definitions/db.pp
+++ manifests/definitions/db.pp
@@ -1,5 +1,4 @@
 define mysql_db (
-	$pass = "$mysql::rootpass",
 	$user = 'root',
 	$host = 'localhost',
 	$perms = 'all privileges'
@@ -11,7 +10,7 @@ define mysql_db (
 			unless => "mysql -uroot -p$mysql::rootpass -h \"$host\" -e 'use $name'";
 
 		"set_perms_$name":
-			command => "mysql -uroot -p$mysql::rootpass -h $host -e \"grant $perms on $name.* to '$user'@'$host' identified by '$pass'\"",
+			command => "mysql -uroot -p$mysql::rootpass -h $host -e \"grant $perms on $name.* to '$user'@'$host'\"",
 			path => '/usr/bin:/bin:/sbin',
 #			unless => "mysql -uroot -p$mysql::rootpass -h $host -e \"select * from information_schema.user_privileges\"|grep $mysql::dbname";
 	}
