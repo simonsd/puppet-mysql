de6960022e642e2ca669084263d90cd5dd3fed10
Dave Simons <dave@inuits.be>
2011-12-05 12:22:01 +0100

    adjust exec name for adding a mysql user
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/definitions/user.pp
+++ manifests/definitions/user.pp
@@ -4,7 +4,7 @@ define mysql_user (
 	$host = 'localhost'
 ){
 	exec {
-		'add_user':
+		"add_mysql_user_${name}":
 			command => "mysql -uroot -p$mysql::rootpass -h \"$host\" -e \"create user '$name'@'$host' identified by '$pass'\"",
 			path => '/usr/bin:/bin:/sbin',
 			unless => "mysql -uroot -p$mysql::rootpass -h$host -e \"use mysql;select user,host from user;\"|grep $host|grep $name"
