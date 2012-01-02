bfe6cfad196d3cfd6cae5ef901e11c8e24c07b5f
Dave Simons <dave@inuits.be>
2012-01-02 08:57:30 +0100

    add rootpass params to both user and db resource defs
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/definitions/db.pp
+++ manifests/definitions/db.pp
@@ -1,17 +1,18 @@
 define mysql_db (
 	$user = 'root',
 	$host = 'localhost',
+	$rootpass = "${mysql::rootpass}",
 	$perms = 'all privileges'
 ){
 	exec {
 		"create_db_$name":
-			command => "mysqladmin -uroot -p$mysql::rootpass -h \"$host\" create \"$name\"",
+			command => "mysqladmin -uroot -p$rootpass -h \"$host\" create \"$name\"",
 			path => '/usr/bin:/bin:/sbin',
-			unless => "mysql -uroot -p$mysql::rootpass -h \"$host\" -e 'use $name'";
+			unless => "mysql -uroot -p$rootpass -h \"$host\" -e 'use $name'";
 
 		"set_perms_$name":
-			command => "mysql -uroot -p$mysql::rootpass -h $host -e \"grant $perms on $name.* to '$user'@'$host'\"",
+			command => "mysql -uroot -p$rootpass -h $host -e \"grant $perms on $name.* to '$user'@'$host'\"",
 			path => '/usr/bin:/bin:/sbin',
-#			unless => "mysql -uroot -p$mysql::rootpass -h $host -e \"select * from information_schema.user_privileges\"|grep $mysql::dbname";
+#			unless => "mysql -uroot -p$rootpass -h $host -e \"select * from information_schema.user_privileges\"|grep $mysql::dbname";
 	}
 }
--- manifests/definitions/user.pp
+++ manifests/definitions/user.pp
@@ -1,12 +1,12 @@
 define mysql_user (
 	$pass = 'StdPas5',
-	$newpass = "",
+	$rootpass = "${mysql::rootpass}",
 	$host = 'localhost'
 ){
 	exec {
 		"add_mysql_user_${name}":
-			command => "mysql -uroot -p$mysql::rootpass -h \"$host\" -e \"create user '$name'@'$host' identified by '$pass'\"",
+			command => "mysql -uroot -p$rootpass -h \"$host\" -e \"create user '$name'@'$host' identified by '$pass'\"",
 			path => '/usr/bin:/bin:/sbin',
-			unless => "mysql -uroot -p$mysql::rootpass -h$host -e \"use mysql;select user,host from user;\"|grep $host|grep $name"
+			unless => "mysql -uroot -p$rootpass -h$host -e \"use mysql;select user,host from user;\"|grep $host|grep $name"
 	}
 }
