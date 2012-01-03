061b3ef0e7cbf1028f25772cfd1e192328f850aa
Dave Simons <dave@inuits.eu>
2012-01-03 11:15:08 +0100

    add server param to db and user defs, to differentiate between which host to create on and which to grant perms to
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/definitions/db.pp
+++ manifests/definitions/db.pp
@@ -1,18 +1,19 @@
 define mysql_db (
 	$user = 'root',
 	$host = 'localhost',
+	$server = 'localhost',
 	$rootpass = "${mysql::rootpass}",
 	$perms = 'all privileges'
 ){
 	exec {
 		"create_db_$name":
-			command => "mysqladmin -uroot -p$rootpass -h \"$host\" create \"$name\"",
+			command => "mysqladmin -uroot -p$rootpass -h \"$server\" create \"$name\"",
 			path => '/usr/bin:/bin:/sbin',
-			unless => "mysql -uroot -p$rootpass -h \"$host\" -e 'use $name'";
+			unless => "mysql -uroot -p$rootpass -h \"$server\" -e 'use $name'";
 
 		"set_perms_$name":
-			command => "mysql -uroot -p$rootpass -h $host -e \"grant $perms on $name.* to '$user'@'$host'\"",
+			command => "mysql -uroot -p$rootpass -h $server -e \"grant $perms on $name.* to '$user'@'$host'\"",
 			path => '/usr/bin:/bin:/sbin',
-#			unless => "mysql -uroot -p$rootpass -h $host -e \"select * from information_schema.user_privileges\"|grep $mysql::dbname";
+#			unless => "mysql -uroot -p$rootpass -h $server -e \"select * from information_schema.user_privileges\"|grep $mysql::dbname";
 	}
 }
--- manifests/definitions/user.pp
+++ manifests/definitions/user.pp
@@ -1,12 +1,13 @@
 define mysql_user (
 	$pass = 'StdPas5',
 	$rootpass = "${mysql::rootpass}",
+	$server = 'localhost',
 	$host = 'localhost'
 ){
 	exec {
 		"add_mysql_user_${name}":
-			command => "mysql -uroot -p$rootpass -h \"$host\" -e \"create user '$name'@'$host' identified by '$pass'\"",
+			command => "mysql -uroot -p$rootpass -h \"$server\" -e \"create user '$name'@'$host' identified by '$pass'\"",
 			path => '/usr/bin:/bin:/sbin',
-			unless => "mysql -uroot -p$rootpass -h$host -e \"use mysql;select user,host from user;\"|grep $host|grep $name"
+			unless => "mysql -uroot -p$rootpass -h$server -e \"use mysql;select user,host from user;\"|grep $host|grep $name"
 	}
 }
